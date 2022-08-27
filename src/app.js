955fdd49db8eef30b0528304b75125f8ecba07b1
Svyatoslav <exemple@gmail.com>
2022-08-27 12:12:45 +0300

    add contextMenu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
 import './styles.css'
+import { ContextMenu } from './menu'
+import { BackgroundModule } from './modules/background.module'
 
+const contextMenu = new ContextMenu()
+
+contextMenu.callUpMenu()
+contextMenu.runModule()
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,39 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu'
 
 export class ContextMenu extends Menu {
+	constructor() {
+		super('body')
+		this.menu = document.querySelector('.menu')
+	}
 
+	callUpMenu() {
+		this.el.addEventListener('contextmenu', e => {
+			e.preventDefault()
+			this.open(e.clientX, e.clientY)
+		})
+
+		this.el.addEventListener('click', e => {
+			e.preventDefault()
+			this.close()
+		})
+	}
+
+	open(posX, posY) {
+		this.menu.style.cssText = `
+    position: absolute;
+    left: ${posX}px;
+    top: ${posY}px;
+    `
+		this.menu.classList.add('open')
+	}
+
+	close() {
+		this.menu.classList.remove('open')
+	}
+
+	add(metod) {
+		this.menu.insertAdjacentHTML('beforeend', metod)
+	}
+
+	runModule() {}
 }
